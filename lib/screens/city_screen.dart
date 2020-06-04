5c397cd899ab9ba07d295b8804d25dbd47f127b0
sharjeelyunus <sharjeel924@gmail.com>
2020-06-05 01:28:23 +0500

    Creating + Styling TextField Widget

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -33,7 +33,13 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  style: TextStyle(color: Colors.black),
+                  decoration: kTextFieldInputDecoration,
+                  onChanged: (value) {
+                    print(value);
+                  },
+                ),
               ),
               FlatButton(
                 onPressed: () {},
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clima/screens/city_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 import 'package:clima/services/weather.dart';
@@ -75,7 +76,16 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CityScreen();
+                          },
+                        ),
+                      );
+                    },
                     child: Icon(
                       Icons.location_city,
                       size: 50.0,
