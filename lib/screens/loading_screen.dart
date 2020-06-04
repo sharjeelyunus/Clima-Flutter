fda05f8d710a664039781b3773311c6a822b4a9b
sharjeelyunus <sharjeel924@gmail.com>
2020-06-04 18:33:22 +0500

    📦 NEW: Request data from API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,6 +12,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    print('this line of code is triggered');
   }
 
   void getLocation() async {
@@ -21,8 +22,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+     http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+     
+     if (response.statusCode == 200) {
+       String data = response.body;
+       print(data);
+     } else {
+       print(response.statusCode);
+     }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
